b38fb4cfa7dd6519b00b8e9a41aba9e57a3b041f
rakzool <60981094+rakzool@users.noreply.github.com>
2021-06-25 16:57:54 +0530

    home section 1.0.0

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import styles from "./home.module.scss";
 
 
 export default function Home() {
-    return (<div>
-        <h1>Home component</h1>
+    return (<div className={styles.mainBody}>
+        <div className={styles.banner}>
+            <h1 className={styles.headingOne}><span>STOP</span> The Spread</h1>
+            <h1 className={styles.headingTwo}>Stay Home Stay <span>SAFE</span></h1>
+        </div>
     </div>)
 }
